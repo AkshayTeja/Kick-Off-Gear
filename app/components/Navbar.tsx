068690517a00176892cb0ff9b2ec88cbb348e77b
Axay17 <akshayteja04@gmail.com>
2024-06-13 21:31:31 +0530

    Navbar Styling added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,14 +6,14 @@ const Navbar = () => {
     <div className="hidden lg:block">
         <div className="container">
             <div className="flex w-fit gap-10 mx-auto font-medium py-4 text-blackish">
-                <Link href="#">HOME</Link>
-                <Link href="#">CATEGORIES</Link>
-                <Link href="#">MEN&apos;S</Link>
-                <Link href="#">WOMEN&apos;S</Link>
-                <Link href="#">EURO &apos;24</Link>
-                <Link href="#">RETRO</Link>
-                <Link href="#">CLUBS</Link>
-                <Link href="#">HOT OFFERS</Link>
+                <Link className='navbar__link relative' href="#">HOME</Link>
+                <Link className='navbar__link relative' href="#">CATEGORIES</Link>
+                <Link className='navbar__link relative' href="#">MEN&apos;S</Link>
+                <Link className='navbar__link relative' href="#">WOMEN&apos;S</Link>
+                <Link className='navbar__link relative' href="#">EURO &apos;24</Link>
+                <Link className='navbar__link relative' href="#">RETRO</Link>
+                <Link className='navbar__link relative' href="#">CLUBS</Link>
+                <Link className='navbar__link relative' href="#">HOT OFFERS</Link>
             </div>
         </div>
     </div>
