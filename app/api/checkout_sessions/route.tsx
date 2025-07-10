2a61b09a2f756e902b15660b2556327e0498f4f5
Axay17 <akshayteja04@gmail.com>
2025-07-11 01:08:39 +0530

    Deployment Error fixed

--- app/api/checkout_sessions/route.tsx
+++ app/api/checkout_sessions/route.tsx
@@ -7,7 +7,7 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2024-06-20",
 });
 
