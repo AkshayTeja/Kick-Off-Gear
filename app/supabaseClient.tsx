84dcf9e163d342c0761707fdbeff807f91035cfc
Axay17 <akshayteja04@gmail.com>
2025-07-11 11:43:33 +0530

    Auth Error fixed

--- app/supabaseClient.tsx
+++ app/supabaseClient.tsx
@@ -1,9 +1,26 @@
+// lib/supabase.js
 import { createClient } from "@supabase/supabase-js";
-import type { SupabaseClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
+
+export async function getCurrentUser() {
+  try {
+    const {
+      data: { user },
+      error,
+    } = await supabase.auth.getUser();
+    if (error) {
+      console.error("Error fetching user:", error.message);
+      return null; // Handle guest/unauthenticated users
+    }
+    return user;
+  } catch (err) {
+    console.error("Unexpected error:", err);
+    return null;
+  }
+}
 
 export default supabase;
